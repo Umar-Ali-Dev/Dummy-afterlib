95f206847d3858ebe09180a57a94f624bd3d8d71
Nabeel Devclan <nabeel.devclan@gmail.com>
2025-12-12 08:51:10 +0500

    ui

--- src/lib/components/landing/FullStory.svelte
+++ src/lib/components/landing/FullStory.svelte
@@ -1,3 +1,10 @@
+<script>
+  import greenBg from '$lib/assets/images/GreenBg3.png';
+
+</script>
+
+
+
 <section class="bg-primary">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col gap-16 py-32">
         <div class="flex justify-center items-center flex-col gap-8">
@@ -23,8 +30,11 @@
             </div>
         </div>
     </div>
-
-    <div class="flex flex-col justify-center items-center p-16 bg-[#02a758]">
+ <div 
+    class="relative max-w-7xl mx-auto py-20 md:py-28 overflow-hidden bg-cover bg-center bg-no-repeat"
+    style="background-image: url({greenBg});"
+  >
+    <div class="flex flex-col justify-center items-center p-16 ">
         <div class="max-w-3xl w-full p-14 flex flex-col justify-start items-stretch shadow-[inset_0_0_12px_6px_rgba(0,0,0,0.16)] bg-white gap-10">
             <div class="font-plus-jakarta-sans text-2xl text-left text-[#1e1e1e]">
                 What started in-house is now the standard for serious marketers worldwide and the
@@ -34,10 +44,11 @@
                 <p class="font-plus-jakarta-sans text-[32px] font-bold text-left text-[#1e1e1e]">
                     Actions speak louder than words
                 </p>
-                <button class="self-start font-plus-jakarta-sans text-lg font-semibold text-left text-[#02a758] hover:underline">
+                <button class="self-start font-plus-jakarta-sans text-lg font-semibold text-left text-[#02a758] underline">
                     Read full story
                 </button>
             </div>
+        </div>
         </div>
     </div>
 </section>
